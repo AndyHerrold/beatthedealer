fb5388daa49900d46c3967ee02606932d70b08de
AndyHerrold <APHERROLD@GMAIL.COM>
2024-11-22 11:53:21 -0600

    Add files via upload

--- andy herrold_beat the dealer.py
+++ andy herrold_beat the dealer.py
@@ -8,4 +8,15 @@ beatthedealer.py
 final project
 AndyHerrold
 """
-
+# I need to figure out how to assign SVG card to player and dealer hand
+class playerHand(simpleGE.Sprite):
+    def__init__(self, scene):
+        super()__init__(scene)
+        self.setImage("?")
+        self.setSize(25, 25)
+        self.reset()
+class Taxi(simpleGE.Sprite):
+    def__init__(self, scene):
+        super().__init__(scene)
+        self.setImage("?")
+        self.setSize(25, 25)
