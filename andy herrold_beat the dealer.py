37dabb1aa20a7b8cfe03493c7b83561de1ca75b8
AndyHerrold <APHERROLD@GMAIL.COM>
2024-12-04 13:12:59 -0600

    Add files via upload

--- andy herrold_beat the dealer.py
+++ andy herrold_beat the dealer.py
@@ -8,6 +8,57 @@ beatthedealer.py
 final project
 AndyHerrold
 """
+#Add constants from previous card game
+NUMCARDS = 52
+RANKNAME = ("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Jack", "Queen", "King",)
+
+FILE_RANK_NAME = ("ace", "2", "3", "3", "5", "6", "7", "8", "9", "10", "jack",
+                  "queen", "king")
+
+SUITNAME = ("clubs", "hearts", "spades", "diamonds")
+STATE = ("deck", "showing", "discarded")
+
+DECK = 0
+SHOWING = 1
+DISCARDED = 2
+
+class Game(simplGE.Scene):
+    def__init__(self):
+        super().__init__()
+        self.cardSlot = CardSlot(self)
+        self.cardSlot.position = (320, 240) #check
+        self.setupCards()
+        self.sprites = [self.cardSlot]
+        
+    def setupCards(self):
+        self.cards []
+        for cn in range(NUMCARDS):
+            self.cards.append(Card(cn))
+class CardSlot(simpleGE.Sprite):
+    """visual element displaying card sprite
+        one for every position on table"""
+    def__init__(self.scene):
+        super().__init__(scene)
+        self.setImage("svg_playing_cards/backs/fish.svg")
+        
+    def process(self):
+        """selects a new card when clicked"""
+        if self.clicked
+            cardNum = random.randrange(NUMCARDS)
+            currentCard = self.scene.cards[cardNum]
+            self.copyImage(currentCard.image)
+            
+class Card(object):
+    def__init__(self, cardNum):
+        super().__init__()
+        
+        self.id = cardNum
+        self.suit = cardNum // 13
+        self.rank = cardNum % 13
+        
+        self.suitName = SUITNAME[self.suit]
+        self.rankName = RANKNAME[self.rank]
 # I need to figure out how to assign SVG card to player and dealer hand
 class playerHand(simpleGE.Sprite):
     def__init__(self, scene):
@@ -37,7 +88,7 @@ class stndBtn(simpleGE.Sprite)
     def_init_(self):
         super().__init__()
         self.text = "Hit"
-        
+       
         
         
     
